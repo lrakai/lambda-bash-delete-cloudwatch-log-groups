2a183cfe52a222010d2e4debce053ffe6475e61c
lrakai <lmrakai@gmail.com>
2019-04-24 21:08:09 -0600

    Full handler script

--- script.sh
+++ script.sh
@@ -1,3 +1,12 @@
+function handler () {
+    set -e
+
+    # Event Data is sent as the first parameter
+    EVENT_DATA=$1
+
+    # Example of command usage
+    EVENT_JSON=$(echo $EVENT_DATA | jq .)
+
     aws logs describe-log-groups --query 'logGroups[*].logGroupName' --output table |
     awk '{print $2}' | 
     grep -v ^$ |
@@ -6,3 +15,7 @@
         echo deleting log group $x
         aws logs delete-log-group --log-group-name $x 2>&1
     done
+
+    # This is the return value because it's being sent to stderr (>&2)
+    echo "{\"success\": true}" >&2
+}
